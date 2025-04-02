637a8c5c3eb190f5852e790c7d02c13736fe65a3
aimetu <41498149+aimetu@users.noreply.github.com>
2025-04-02 16:12:11 +0800

    Update diy-part2.sh

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -55,10 +55,10 @@ sed -i "s/12345678/password/g" target/linux/qualcommax/base-files/etc/uci-defaul
 # git clone https://github.com/sirpdboy/luci-app-advancedplus.git package/luci-app-advancedplus
 
 # 删除自带 AdguardHome 文件，添加 AdguardHome 广告过滤插件
-rm -rf feeds/packages/net/adguardhome
+# rm -rf feeds/packages/net/adguardhome
 # rm -rf feeds/luci/applications/luci-app-adguardhome
 # git clone https://github.com/xptsp/luci-app-adguardhome package/luci-app-adguardhome
-git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+# git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
 # 更新 golang 依赖（ mosdns & alist 插件 )
