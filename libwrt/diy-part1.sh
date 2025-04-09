af3f2991e86ee133bf651ae3e9594aed71ea545b
aimetu <41498149+aimetu@users.noreply.github.com>
2025-04-09 13:57:08 +0800

    Update diy-part1.sh

--- libwrt/diy-part1.sh
+++ libwrt/diy-part1.sh
@@ -14,7 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
 # echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
